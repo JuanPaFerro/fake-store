6da69ba6b9d97488eb350d5df4c66f21a96d6ebc
JuanPaFerro <juanpabloferroarroyo@gmail.com>
2022-06-26 23:08:45 -0400

    created a first version of the ProductCard component

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React from "react";
 
-const ProductCard = ({id, title, price, description, category, image, rating}) => {
+const ProductCard = ({
+  id,
+  title,
+  price,
+  description,
+  category,
+  image,
+  rating,
+}) => {
   return (
-    <li className='flex flex-col'>
-        <p>{id}</p>
+    <div className="flex h-96 w-10/12 flex-col space-y-2 lg:w-3/12 lg:py-4">
+      <div className="h-full border-b-2 bg-gradient-to-b from-black to-stone-900 overflow-auto">
+        <img src={image} alt={description} className="h-full w-full object-cover object-top"/>
+      </div>
+      <div className="flex justify-between text-xl font-black text-white">
         <p>{title}</p>
-        <p>{price}</p>
-        <p>{description}</p>
-        <p>{category}</p>
-        <p>{image}</p>
-    </li>
-  )
-}
+        <p>
+          {Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+          }).format(price)}
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
