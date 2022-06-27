59657fe5d658f6d3b48ea2deb89e18bf6492ca01
JuanPaFerro <juanpabloferroarroyo@gmail.com>
2022-06-26 23:09:26 -0400

    ProductList component

--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -5,8 +5,7 @@ const ProductsList = () => {
   const { products, loading } = useGetAllProducts();
 
   if (loading) return;
-
-  return <ul>{products?.map(product => <ProductCard key={product.id} {...product}></ProductCard> )}</ul>;
+  return <div className="flex flex-col items-center py-8 space-y-8 lg:flex-row lg:space-x-4 lg:flex-wrap lg:justify-evenly lg:space-y-0">{products?.map(product => <ProductCard key={product.id} {...product}></ProductCard> )}</div>;
 };
 
 export default ProductsList;
